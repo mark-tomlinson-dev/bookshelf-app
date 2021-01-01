aac9adeb17f39fb74d2f91ecd6240df31e69f14c
Mark Tomlinson <mark.tomlinson3@gmail.com>
2021-01-01 11:33:20 +1100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios';
 
-function App() {
+const { REACT_APP_API_KEY } = process.env;
+
+const url = `https://www.googleapis.com/books/v1/volumes?q=philosophy&orderBy=relevance&key=${REACT_APP_API_KEY}`
+
+const App = () => {
+  const [books, setBooks] = useState([])
+  console.log(books);
+
+  useEffect(() => {
+    axios
+      .get(url)
+      .then(res => {
+        const filteredBooks = new Set(res.data.items);
+        const arrayOfFilteredBooks = [...filteredBooks];
+        setBooks(arrayOfFilteredBooks)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main>
+        <h1>Bookshelf App</h1>
+          {books.map(book => (
+            <div key={book.id}>
+              <p><strong>{book.volumeInfo.title}</strong> by {book.volumeInfo.authors}</p>
+              <img alt="" src={book.volumeInfo.imageLinks.thumbnail}/>
+            </div>
+          ))}
+      </main>
     </div>
   );
 }
